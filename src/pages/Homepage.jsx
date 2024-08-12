5510e0a5330ac00368ffda74a40a05c07179126b
Lee gangho <rkdgh1190@gmail.com>
2024-08-12 12:39:06 +0900

    테스트 완료

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 
 const Homepage = () => {
-  throw new Error("This is a forced error for testing the ErrorBoundary.");
-  // 에러 바운더리 테스트용 나중에 지워주세요
-
-  return <div>Welcome to the Homepage</div>;
+  return <div>홈페이지</div>;
 };
 
 export default Homepage;
