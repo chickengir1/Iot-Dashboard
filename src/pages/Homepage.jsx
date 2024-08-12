ba55aff473eabb6fa5a1e76494f1301d38c5f1a1
Lee gangho <rkdgh1190@gmail.com>
2024-08-12 12:36:12 +0900

    에러 바운더리 테스트

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 const Homepage = () => {
-  return <div></div>;
+  throw new Error("This is a forced error for testing the ErrorBoundary.");
+  // 에러 바운더리 테스트용 나중에 지워주세요
+
+  return <div>Welcome to the Homepage</div>;
 };
 
 export default Homepage;
