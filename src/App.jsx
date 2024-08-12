cbc9484eef32d0f153e12f606ea72eeed8772584
Lee gangho <rkdgh1190@gmail.com>
2024-08-12 14:24:08 +0900

    루트 리듀서 템플릿 추ㅏㄱ

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Homepage";
 import ErrorBoundary from "./error/ErrorBoundary";
+import Login from "./pages/Loginpage";
 
 function App() {
   return (
     <ErrorBoundary>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Login />} />
         </Routes>
       </Router>
     </ErrorBoundary>
---
+++ src/redux/rootReducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+  // 리듀서 추가
+});
+
+export default rootReducer;
