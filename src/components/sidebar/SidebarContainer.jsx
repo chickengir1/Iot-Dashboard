5f7c4fde7b2c0929230c1de207c989562db713bd
Lee gangho <rkdgh1190@gmail.com>
2024-08-12 14:26:59 +0900

    사이드바 추가

---
+++ src/components/sidebar/SidebarContainer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const sidebarcontainer = () => {
+  return <div></div>;
+};
+
+export default sidebarcontainer;
---
+++ src/components/sidebar/SidebarUi.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const sidebarUi = () => {
+  return <div></div>;
+};
+
+export default sidebarUi;
