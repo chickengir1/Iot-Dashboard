d0f922bc499d0f204f57ac33a777189e7ac6b0bf
Daram0o0 <c30728535@gmail.com>
2024-08-13 14:36:14 +0900

    모바일 뉴스레터 페이지 완성

--- src/pages/NewsListPage.jsx
+++ src/pages/NewsListPage.jsx
@@ -9,12 +9,12 @@ import {
 } from "@mui/material";
 import Sidebar from "../components/sidebar/sidebarcontainer";
 
-const bigNews = [
+const datas = [
   {
     image: ".",
     title: "Lizard",
     content: `Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica`,
+            species, ranging across all continents except Antarctica efewfwef efwe fwe fwef wef weffwe fw efw efw `,
   },
   {
     image: "https://images.unsplash.com/photo-1527549993586-dff825b37782",
@@ -38,9 +38,9 @@ const bigNews = [
   },
 ];
 
-const BigCardNews = ({ image, title, content }) => {
+const BigNewsCard = ({ image, title, content }) => {
   // 카드 작명 구림. 카드 내부 배치 비율 조절
-  const bigcardStyle = {
+  const bigCardStyle = {
     display: "flex",
     alignItems: "center",
     padding: 1,
@@ -49,21 +49,44 @@ const BigCardNews = ({ image, title, content }) => {
     maxWidth: 345,
     minWidth: 345,
   };
+
+  const titleStyle = {
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    height: 30,
+  };
+
+  const contentStyle = {
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    display: "-webkit-box",
+    WebkitLineClamp: 3,
+    WebkitBoxOrient: "vertical",
+    height: 60,
+  };
+
   return (
-    <Card sx={bigcardStyle}>
+    <Card sx={bigCardStyle}>
       <CardActionArea>
         <CardMedia
           component="img"
-          height="140"
-          sx={{ border: "solid 1px #d9d9d9" }}
+          sx={{
+            width: "100%",
+            height: 140,
+            border: "solid 1px #d9d9d9",
+            objectFit: "cover",
+          }}
           image={image}
           alt="green iguana"
         />
+
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+          <Typography gutterBottom variant="h5" component="div" sx={titleStyle}>
             {title}
           </Typography>
-          <Typography variant="body2" color="text.secondary">
+
+          <Typography variant="body2" color="text.secondary" sx={contentStyle}>
             {content}
           </Typography>
         </CardContent>
@@ -72,7 +95,68 @@ const BigCardNews = ({ image, title, content }) => {
   );
 };
 
-export const MobileDeviceList = () => {
+const SmallNewsCard = ({ image, title, content }) => {
+  // 카드 작명 구림. 카드 내부 배치 비율 조절
+  const smallCardStyle = {
+    display: "flex",
+    flexDirection: "row",
+    padding: 1,
+    borderRadius: 2,
+    boxShadow: 1,
+    width: "100%",
+  };
+
+  const titleStyle = {
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    height: 30,
+  };
+
+  const contentStyle = {
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    display: "-webkit-box",
+    WebkitLineClamp: 4,
+    WebkitBoxOrient: "vertical",
+    height: 80,
+  };
+
+  return (
+    <Card sx={smallCardStyle}>
+      <CardActionArea sx={{ display: "flex", alignItems: "center" }}>
+        <CardMedia
+          component="img"
+          sx={{
+            width: 140,
+            height: 140,
+            border: "solid 1px #d9d9d9",
+            objectFit: "cover",
+            borderRadius: 2,
+          }}
+          image={image}
+          alt="green iguana"
+        />
+
+        <CardContent
+          sx={{
+            flex: 1,
+          }}
+        >
+          <Typography gutterBottom variant="h5" component="div" sx={titleStyle}>
+            {title}
+          </Typography>
+
+          <Typography variant="body2" color="text.secondary" sx={contentStyle}>
+            {content}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+export const MobileNewsList = () => {
   const styles = {
     mobileLayout: {
       padding: 2,
@@ -102,8 +186,8 @@ export const MobileDeviceList = () => {
         핫 토픽!
       </Typography>
       <Box sx={styles.carouseLayout}>
-        {bigNews.map((item, index) => (
-          <BigCardNews
+        {datas.map((item, index) => (
+          <BigNewsCard
             key={index}
             image={item.image}
             title={item.title}
@@ -114,40 +198,29 @@ export const MobileDeviceList = () => {
       <Typography align="center" variant="h5">
         데일리 소식
       </Typography>
-      <Card
+      <Box
         sx={{
           display: "flex",
+          flexDirection: "column",
+          gap: 2,
           alignItems: "center",
-          padding: 1,
-          borderRadius: 2,
-          boxShadow: 1,
-          maxWidth: 345,
-          minWidth: 345,
         }}
       >
-        <CardMedia
-          component="img"
-          height="140"
-          width="140"
-          sx={{ border: "solid 1px #d9d9d9" }}
-          image={"image"}
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {"title"}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {"content"}
-          </Typography>
-        </CardContent>
-      </Card>
+        {datas.map((item, index) => (
+          <SmallNewsCard
+            key={index}
+            image={item.image}
+            title={item.title}
+            content={item.content}
+          />
+        ))}
+      </Box>
       <Sidebar />
     </Box>
   );
 };
 
-export const DesktopDeviceList = () => {
+export const DesktopNewsList = () => {
   return (
     <Box>
       <Sidebar />
@@ -156,7 +229,7 @@ export const DesktopDeviceList = () => {
 };
 const NewsListPage = () => {
   const isDesktop = useMediaQuery("(min-width:1280px)");
-  return isDesktop ? <DesktopDeviceList /> : <MobileDeviceList />;
+  return isDesktop ? <DesktopNewsList /> : <MobileNewsList />;
 };
 
 export default NewsListPage;
