289e1cb82a1f3cc590e36f63446bfd989d41531b
Daram0o0 <c30728535@gmail.com>
2024-08-14 00:45:21 +0900

    데스크탑 투두페이지 추가

--- src/pages/TodoListPage.jsx
+++ src/pages/TodoListPage.jsx
@@ -1,6 +1,7 @@
 import {
   Box,
   Button,
+  Grid,
   IconButton,
   Typography,
   useMediaQuery,
@@ -10,7 +11,6 @@ import UserCard from "../components/usercard/UserCardUi";
 import {
   AddCircleOutlineOutlined,
   CheckBoxOutlineBlank,
-  CheckBoxOutlineBlankOutlined,
   CheckBoxOutlined,
 } from "@mui/icons-material";
 import Sidebar from "../components/sidebar/sidebarUi";
@@ -60,6 +60,29 @@ const styles = {
     alignItems: "center",
     justifyContent: "center",
   },
+  desktopLayout: {
+    padding: 2,
+    margin: "0 auto",
+    display: "flex",
+  },
+  mainContentStyle: {
+    padding: 2,
+    marginLeft: 2,
+    border: "1px solid #ddd",
+    borderRadius: 2,
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    gap: 2,
+  },
+  serveContentStyle: {
+    minWidth: "220px",
+    maxWidth: "220px",
+    marginLeft: 2,
+    border: "1px solid #ddd",
+    padding: 2,
+    borderRadius: 2,
+  },
 };
 
 const TodoComponent = ({ date, description, isFinish }) => {
@@ -74,9 +97,22 @@ const TodoComponent = ({ date, description, isFinish }) => {
         </Typography>
       </Box>
       <IconButton>
-        {isFinish ? <CheckBoxOutlined /> : <CheckBoxOutlineBlankOutlined />}
+        {isFinish ? <CheckBoxOutlined /> : <CheckBoxOutlineBlank />}
       </IconButton>
     </Box>
+  );
+};
+
+const AddTodoButton = () => {
+  return (
+    <Button
+      variant="contained"
+      fullWidth
+      endIcon={<AddCircleOutlineOutlined />}
+      sx={styles.buttonStyle}
+    >
+      할 일 추가하기
+    </Button>
   );
 };
 
@@ -92,21 +128,51 @@ const MobileTodoList = () => (
         isFinish={todo.isFinish}
       />
     ))}
-    <Button
-      variant="contained"
-      fullWidth
-      endIcon={<AddCircleOutlineOutlined />}
-      sx={styles.buttonStyle}
-    >
-      할 일 추가하기
-    </Button>
+    <AddTodoButton />
     <Sidebar />
   </Box>
 );
 
+const DesktopTodoList = () => {
+  return (
+    <Box sx={styles.desktopLayout}>
+      <Sidebar />
+      <Box sx={styles.mainContentStyle}>
+        <Grid container columnSpacing={2}>
+          <Grid item xs={6} sm={6} md={6} lg={6}>
+            <UserCard />
+          </Grid>
+          <Grid
+            item
+            xs={6}
+            sm={6}
+            md={6}
+            lg={6}
+            sx={{ display: "flex", mb: 2 }}
+          >
+            <AddTodoButton />
+          </Grid>
+        </Grid>
+        <Typography textAlign="center" variant="h6" gutterBottom>
+          {"투두 리스트"}
+        </Typography>
+        {todos.map((todo, index) => (
+          <TodoComponent
+            key={index}
+            date={todo.date}
+            description={todo.description}
+            isFinish={todo.isFinish}
+          />
+        ))}
+      </Box>
+      <Box sx={styles.serveContentStyle}>{/* 이미지 아무거나 */}</Box>
+    </Box>
+  );
+};
+
 const TodoListPage = () => {
   const isDesktop = useMediaQuery("(min-width:1280px)");
-  return isDesktop ? <MobileTodoList /> : <MobileTodoList />;
+  return isDesktop ? <DesktopTodoList /> : <MobileTodoList />;
 };
 
 export default TodoListPage;
