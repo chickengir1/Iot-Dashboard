35d5ade397061a9069226673663c1d4282c81ea5
Daram0o0 <c30728535@gmail.com>
2024-08-16 20:10:57 +0900

    notificationreducer 수정

--- src/redux/reducers/notificationReducer.js
+++ src/redux/reducers/notificationReducer.js
@@ -1,6 +1,8 @@
 import {
   INCREMENT_UNREAD_COUNT,
   SET_UNREAD_COUNT,
+  DECREMENT_UNREAD_COUNT,
+  RESET_UNREAD_COUNT,
 } from "../actions/notificationActions";
 
 const initialState = {
@@ -19,12 +21,12 @@ const notificationReducer = (state = initialState, action) => {
         ...state,
         unreadCount: state.unreadCount + 1,
       };
-    case "DECREMENT_UNREAD_COUNT":
+    case DECREMENT_UNREAD_COUNT:
       return {
         ...state,
         unreadCount: state.unreadCount > 0 ? state.unreadCount - 1 : 0,
       };
-    case "RESET_UNREAD_COUNT":
+    case RESET_UNREAD_COUNT:
       return {
         ...state,
         unreadCount: 0,
