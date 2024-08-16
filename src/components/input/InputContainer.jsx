b01d1fc76207e106a3aef37a03fca42a5dd37df7
Lee gangho <rkdgh1190@gmail.com>
2024-08-16 18:50:00 +0900

    각 컴포넌트별 주석 추가

--- src/components/input/InputContainer.jsx
+++ src/components/input/InputContainer.jsx
@@ -1,6 +1,8 @@
 import React, { forwardRef } from "react";
 import InputUi from "./InputUi";
 
+// 나중에 삭제 예정인 컴포넌트임
+
 const InputContainer = forwardRef(({ id, label, placeholder, error }, ref) => (
   <InputUi
     id={id}
--- src/components/selector/SelectContainer.jsx
+++ src/components/selector/SelectContainer.jsx
@@ -1,5 +1,7 @@
 import SelectUi from "./SelectUi";
 
+// 나중에 삭제할거임 이메일 셀렉터 대체
+
 const SelectContainer = ({ id, label, placeholder, error, selectValue }) => (
   <SelectUi
     id={id}
