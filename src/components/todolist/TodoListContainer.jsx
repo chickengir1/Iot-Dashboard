d0f62b592794cfd43ca293b587c9d4e8035d9361
Daram0o0 <c30728535@gmail.com>
2024-08-17 19:00:21 +0900

    homepage -> todolistui에  todolistcontainer에서  필터된 투두 전달

--- src/components/todolist/TodoListContainer.jsx
+++ src/components/todolist/TodoListContainer.jsx
@@ -1,12 +1,20 @@
-import React from "react";
 import TodoListUi from "./TodoListUi";
+import { useSelector } from "react-redux";
 
 const TodoListContainer = () => {
-  return (
-    <>
-      <TodoListUi />
-    </>
-  );
+  const todos = useSelector((state) => state.todo.todos);
+
+  // isFinish가 false이면서 투두 최대 4개까지 필터링
+  const maxTodos = 4;
+  const filteredTodos = todos
+    .filter((todo) => !todo.isFinish)
+    .slice(0, maxTodos);
+
+  const blanks = Array(maxTodos - filteredTodos.length).fill(null);
+
+  const combinedTodos = [...filteredTodos, ...blanks];
+
+  return <TodoListUi todos={combinedTodos} />;
 };
 
 export default TodoListContainer;
