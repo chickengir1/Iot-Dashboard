1d9e16cd6a979f349c48df0cb5358a9e781e0bb2
Lee gangho <rkdgh1190@gmail.com>
2024-08-19 16:10:26 +0900

    포스트 요청 커스텀 훅 임시 주석 추가 (할 일)

--- src/hooks/usePostRequest.jsx
+++ src/hooks/usePostRequest.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import axios from "axios";
 
+// 로딩 관련 처리는 나중에 공용으로 빼고 서버 응답 메세지 및 에러 메세지는 500번일때 에러 바운더리에 위임 클라이언트측 에러일때 노티피케이션 만들어서 위임
+
 const usePostRequest = (url) => {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState(null);
