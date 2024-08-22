c1b0c8590317bfa6d5a7b3e4c5612c2d42233fe1
Lee gangho <rkdgh1190@gmail.com>
2024-08-22 21:39:31 +0900

    유틸 폴더 api 패스 입력

--- src/utils/apiMap.js
+++ src/utils/apiMap.js
@@ -1,5 +1,8 @@
-// api 주소 적어놓는 페이지 아직 미완성
-"/api/auth/register";
-"/api/auth/forgot-password";
-"/api/auth/login";
-"/api/auth/find-id";
+const API_BASE_URL = "/api";
+
+export const API_PATHS = {
+  REGISTER: `${API_BASE_URL}/auth/register`,
+  FINDPW: `${API_BASE_URL}/auth/forgot-password`,
+  FINDID: `${API_BASE_URL}/auth/find-id`,
+  LOGIN: `${API_BASE_URL}/auth/login`,
+};
