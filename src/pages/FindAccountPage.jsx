1d3f0897bf2a822a5202e6276bbbea21b5260cea
Lee gangho <rkdgh1190@gmail.com>
2024-08-23 16:44:25 +0900

    아이디/비밀번호 찾기 페이지 패스 경로 추가

--- src/pages/FindAccountPage.jsx
+++ src/pages/FindAccountPage.jsx
@@ -6,6 +6,7 @@ import {
   DesktopEntryMainLayout,
 } from "../styles/index";
 import FooterLinks from "../components/footerlinks/FooterLinksContainer";
+import { useNavigate } from "react-router-dom";
 
 const styles = {
   findButtonStyle: {
@@ -22,26 +23,34 @@ const styles = {
   imageLayout: { border: "solid 1px #ddd", height: "250px" },
 };
 
-const LookupButtonComponent = () => {
+const LookupButtonComponent = ({ handleNavigation }) => {
   return (
     <>
-      <Button variant="contained" sx={styles.findButtonStyle}>
+      <Button
+        variant="contained"
+        sx={styles.findButtonStyle}
+        onClick={() => handleNavigation("ID")}
+      >
         아이디 찾기
       </Button>
-      <Button variant="contained" sx={styles.findButtonStyle}>
+      <Button
+        variant="contained"
+        sx={styles.findButtonStyle}
+        onClick={() => handleNavigation("PW")}
+      >
         비밀번호 찾기
       </Button>
     </>
   );
 };
 
-export const MobileLookup = () => {
+export const MobileLookup = ({ handleNavigation }) => {
   return (
     <MobileEntryLayout>
       <Box sx={styles.imageLayout}>
         <img alt="이미지" />
       </Box>
-      <LookupButtonComponent />
+      <LookupButtonComponent handleNavigation={handleNavigation} />
       <FooterLinks
         text1={"아이디/비밀번호 찾기"}
         link1={"/find-account"}
@@ -52,14 +61,14 @@ export const MobileLookup = () => {
   );
 };
 
-export const DesktopLookup = () => {
+export const DesktopLookup = ({ handleNavigation }) => {
   return (
     <DesktopEntryLayout>
       <DesktopEntryMainLayout>
         <Box sx={styles.imageLayout}>
           <img alt="이미지" />
         </Box>
-        <LookupButtonComponent />
+        <LookupButtonComponent handleNavigation={handleNavigation} />
         <FooterLinks
           text1={"아이디/비밀번호 찾기"}
           link1={"/find-account"}
@@ -72,9 +81,23 @@ export const DesktopLookup = () => {
 };
 
 const FindAccountPage = () => {
+  const navigate = useNavigate();
+
+  const handleNavigation = (apiType) => {
+    if (apiType === "ID") {
+      navigate("/find-userid");
+    } else if (apiType === "PW") {
+      navigate("/forgot-password");
+    }
+  };
+
   const isDesktop = useMediaQuery("(min-width:600px)");
 
-  return isDesktop ? <DesktopLookup /> : <MobileLookup />;
+  return isDesktop ? (
+    <DesktopLookup handleNavigation={handleNavigation} />
+  ) : (
+    <MobileLookup handleNavigation={handleNavigation} />
+  );
 };
 
 export default FindAccountPage;
--- src/utils/apiMap.js
+++ src/utils/apiMap.js
@@ -3,6 +3,6 @@ const API_BASE_URL = "/api";
 export const API_PATHS = {
   REGISTER: `${API_BASE_URL}/auth/register`,
   FINDPW: `${API_BASE_URL}/auth/forgot-password`,
-  FINDID: `${API_BASE_URL}/auth/find-id`,
+  FINDID: `${API_BASE_URL}/auth/find-userid`,
   LOGIN: `${API_BASE_URL}/auth/login`,
 };
