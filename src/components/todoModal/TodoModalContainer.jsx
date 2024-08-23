67838deda2fc283054dc5b895ed4cea6d9680f90
Daram0o0 <c30728535@gmail.com>
2024-08-24 07:18:02 +0900

    getFormattedDate 유틸파일로 분리

--- src/components/todoModal/TodoModalContainer.jsx
+++ src/components/todoModal/TodoModalContainer.jsx
@@ -6,16 +6,11 @@ import { get, save } from "../../utils/localStorage";
 import { todoFields as fields } from "../../utils/formFields";
 import { getResponseMessage } from "../../error/getResponseMessage";
 import { handleFormSubmit } from "../../utils/handleSubmit";
+import { getFormattedDate } from "../../utils/dateUtils";
 
 const TodoModalContainer = ({ setNotification, todos, setTodos }) => {
   const dispatch = useDispatch();
   const combined = useForm();
-
-  const getFormattedDate = () => {
-    const date = new Date();
-    const koreanDate = new Date(date.getTime() + 9 * 60 * 60 * 1000);
-    return koreanDate.toISOString().split("T")[0];
-  };
 
   const onSubmit = (formValues) => {
     const formData = {
@@ -48,7 +43,6 @@ const TodoModalContainer = ({ setNotification, todos, setTodos }) => {
 
     if (todoresponse) {
       const updatedTodos = [...todos, formData];
-      console.log("updatedTodos", updatedTodos);
       setTodos(updatedTodos);
       save("todos", updatedTodos);
       dispatch(setModalType());
