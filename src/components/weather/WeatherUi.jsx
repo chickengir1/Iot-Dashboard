05988084d245b61b2c44360c12fd2ce612c160cc
Daram0o0 <c30728535@gmail.com>
2024-08-26 20:04:48 +0900

    icon 불러오기

--- src/components/weather/WeatherUi.jsx
+++ src/components/weather/WeatherUi.jsx
@@ -5,10 +5,19 @@ const WeatherUi = ({ weatherData }) => {
     <Box sx={{ mb: 2, minHeight: "20vh" }}>
       <Card sx={{ mt: 2, minHeight: "20vh" }}>
         <CardContent>
-          <Typography variant="subtitle1">날씨 위젯 ui</Typography>
-          <Typography>
-            {weatherData ? JSON.stringify(weatherData) : "nn"}
-          </Typography>
+          {weatherData && (
+            <Box>
+              <Typography variant="location">
+                {weatherData.country},{weatherData.locationName}
+              </Typography>
+              <img src={weatherData.weatherIcon} />
+              <Typography>{weatherData.weatherDesc}</Typography>
+              <Typography>{weatherData.currentTemp}</Typography>
+              <Typography>
+                {weatherData.minTemp}/{weatherData.maxTemp}
+              </Typography>
+            </Box>
+          )}
         </CardContent>
       </Card>
     </Box>
