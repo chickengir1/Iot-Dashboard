f4409b7096f223277796b844b5139e2dd3f42ce9
Daram0o0 <c30728535@gmail.com>
2024-08-27 17:51:00 +0900

    todopage 비율 조절

--- src/pages/todo/TodoUi.jsx
+++ src/pages/todo/TodoUi.jsx
@@ -22,8 +22,14 @@ const TodoUi = ({ isDesktop, setNotification, todos, setTodos, onOpen }) => {
         <MainLayout>
           <UserCard />
           <Box sx={{ flex: "1 1 100px", aspectRatio: "1/1" }}>
-            <Typography variant="subtitle1">투두 리스트</Typography>
-            <TodoListContainer todos={todos} setTodos={setTodos} />
+            <Typography variant="subtitle1" gutterBottom>
+              투두 리스트
+            </Typography>
+            <TodoListContainer
+              todos={todos}
+              setTodos={setTodos}
+              height={"60vh"}
+            />
           </Box>
           <BlueRoundedButton
             variant="contained"
