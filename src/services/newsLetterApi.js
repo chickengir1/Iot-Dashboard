ec89aaa05829304f483e53f20ffd3b3c0189fb93
Lee gangho <rkdgh1190@gmail.com>
2024-09-04 14:26:26 +0900

    테스트

--- src/services/newsLetterApi.js
+++ src/services/newsLetterApi.js
@@ -7,7 +7,9 @@ export const getNewsletterData = async (
   rowsPerPage = 10
 ) => {
   try {
-    const serviceKey = import.meta.env.VITE_NEWS_API_KEY;
+    const serviceKey =
+      import.meta.env.VITE_NEWS_API_KEY ??
+      "K1En4lCOFe4MNyzruwS6qdiUcRJDAeC3K2DwskjKAMIYG1www1SJgZbI1xC2k7cDTMMnRKRgWCWE5i8lcLaJ/w==";
 
     const response = await axios.get("/open-api/B500001/drghtNewInfo/list", {
       params: {
