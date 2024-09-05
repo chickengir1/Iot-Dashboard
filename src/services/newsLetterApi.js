0a53acdb9b51c04a5b842e5e1d70e18718cae2f8
Lee gangho <rkdgh1190@gmail.com>
2024-09-05 14:49:05 +0900

    123

--- src/services/newsLetterApi.js
+++ src/services/newsLetterApi.js
@@ -8,7 +8,6 @@ export const getNewsletterData = async (
 ) => {
   try {
     const serviceKey =
-      import.meta.env.VITE_NEWS_API_KEY ??
       "K1En4lCOFe4MNyzruwS6qdiUcRJDAeC3K2DwskjKAMIYG1www1SJgZbI1xC2k7cDTMMnRKRgWCWE5i8lcLaJ/w==";
 
     const response = await axios.get("/open-api/B500001/drghtNewInfo/list", {
